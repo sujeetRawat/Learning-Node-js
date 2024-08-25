0663db565efd8c2dcea805103a56507df0ac53b8
sujeetRawat <sujeetrawat890@gmail.com>
2024-08-25 21:11:02 +0530

    Adding different routes

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-console.log("hello world ");
-
 const http = require('http'); 
+const fs = require('fs');
 
 const server = http.createServer((req,res) => {
-    res.end("My name is Sujeet");
+    const url = req.url;
+
+    if(url == '/home'){
+        res.write('<h1>Welcome Home </h1>');
+    } else if(url == '/about'){
+        res.write('<h1>Welcome to About us page </h1>');
+    }else if(url == '/node'){
+            res.write('<h1>Welcome to my Node Js Project </h1>');
+    } else {
+        res.write('<h1>Welcome!! </h1>')
+        }
 })
 
 server.listen(4000, ()=> {
